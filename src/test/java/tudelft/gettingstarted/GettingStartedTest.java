8de1f73767fe7369c645683f51b021892eb99ebd
Frank-Tachapon <ftachapon@gmail.com>
2026-05-14 16:08:31 +0700

    Complete the test class for addFive

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -5,23 +5,24 @@ import org.junit.jupiter.api.Test;
 
 public class GettingStartedTest {
 
+    // เทสที่ 1: อันนี้มีให้อยู่แล้ว (ทดสอบ 20+5 = 25)
     @Test
-    public void addFiveTo20() {
+    public void addFiveToTwenty() {
         int result = new GettingStarted().addFive(20);
-        Assertions.assertEquals(25,result);
+        Assertions.assertEquals(25, result);
     }
 
-//    UNCOMMENT THE CODE BELOW, AND FILL THE GAPS!
+    // เทสที่ 2: เติมเลขให้สมบูรณ์ (ทดสอบ 0+5 = 5)
+    @Test
+    public void addFiveToZero() {
+        int result = new GettingStarted().addFive(0);
+        Assertions.assertEquals(5, result);
+    }
 
-//    @Test
-//    public void addFiveToZero() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(???, result);
-//    }
-//
-//    @Test
-//    public void addFiveToMinus20() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(????,result);
-//    }
+    // เทสที่ 3: เขียนเองทั้งหมด (ทดสอบ -20+5 = -15)
+    @Test
+    public void addFiveToMinusTwenty() {
+        int result = new GettingStarted().addFive(-20);
+        Assertions.assertEquals(-15, result);
+    }
 }
